c3d400e85fa97082cc812575dbb8e3c3e253d30c
wei <171336915@qq.com>
2018-01-09 16:28:52 +0800

    初始化框架

---
+++ Application/Runtime/Cache/Home/2a94b8bda04ff446dcedd42276fc99ee.php
@@ -0,0 +1,10 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>php数组</title>
+</head>
+<body>
+11111111111
+</body>
+</html>
