13a784c2cfa00f0a0f45a654abfa6420a1666661
wei <171336915@qq.com>
2018-01-09 16:52:53 +0800

    数据链链接信息

---
+++ Application/Common/Conf/db.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mr.Wei
+ * Date: 2018/1/9
+ * Time: 16:32
+ */
+
+    return array(
+        'DB_TYPE'               =>  'mysqli',     // 数据库类型
+        'DB_HOST'               =>  'localhost', // 服务器地址
+        'DB_NAME'               =>  'weixin',          // 数据库名
+        'DB_USER'               =>  'root',      // 用户名
+        'DB_PWD'                =>  'root',          // 密码
+        'DB_PORT'               =>  '3306',        // 端口
+        'DB_PREFIX'             =>  'sdsy_',    // 数据库表前缀
+    );
